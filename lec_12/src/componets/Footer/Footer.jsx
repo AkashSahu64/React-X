d8ffb987a87adba18a4c9bd1e192adc27c770c1e
AkashSahu64 <aakashsahu9415954491@gmail.com>
2024-05-21 18:44:17 +0530

    Changing in footer

--- lec_12/src/componets/Footer/Footer.jsx
+++ lec_12/src/componets/Footer/Footer.jsx
@@ -36,12 +36,12 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/AkashSahu64"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
                                     >
-                                        Github
+                                        Github 
                                     </a>
                                 </li>
                                 <li>
@@ -71,9 +71,9 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        © 2024
+                        <a href="https://akashsahu.com/" className="hover:underline">
+                             akashsahu
                         </a>
                         . All Rights Reserved.
                     </span>
