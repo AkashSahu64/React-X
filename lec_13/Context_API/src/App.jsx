908c3ac4b8f3e224ba88b1c84491840926aad9db
AkashSahu64 <aakashsahu9415954491@gmail.com>
2024-05-22 17:02:08 +0530

    Context API Project Done

---
+++ lec_13/Context_API/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './componets/Login'
+import Profile from './componets/Profile'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+     <Login/> 
+     <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
